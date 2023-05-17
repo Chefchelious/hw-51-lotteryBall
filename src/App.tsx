aff1e53fd797ccddd4aa140a615eb0071bc72d2d
Dmitrii Morrows <trollman1993@gmail.com>
2023-05-17 22:00:01 +0600

    the basic logic was spelled out.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import React, {useState} from 'react';
 import Ball from "./RandomBall/RandomBall";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+  const [numbers, setNumbers] = useState<number[]>([]);
 
-    </div>
+  const changeNumber = () => {
+    const numbers: number[] = [];
+
+    while (numbers.length < 5) {
+      const randomNumber: number = Math.floor(Math.random() * 32) + 5;
+
+      if(!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber);
+      }
+    }
+    numbers.sort((a: number, b: number) => a - b);
+    setNumbers(numbers);
+  };
+  return (
+      <div className="App">
+        <div className="container">
+          {numbers.map((number:number, index: number) => (<Ball key={index} number={number} />))}
+        </div>
+
+        <div>
+          <button onClick={changeNumber}>Click</button>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
